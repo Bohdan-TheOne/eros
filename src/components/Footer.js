d81d8327169c40fe864591d279529f7604366498
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-04-23 18:51:58 +0300

    Add Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li>555-555-555</li>
+                    <li><a href='https://github.com/Bohdan-TheOne/eros'><img src='https://github.com/fluidicon.png'/> GitHub page</a></li>
+                    <li><a href="/about"></a>About Us</li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/current">Current</a></li>
+                    <li><a href="/statistic">Statistic</a></li>
+                    <li><a href="/about">About Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
