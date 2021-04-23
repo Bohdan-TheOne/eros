34b98fc78745d3808c92525f82383f73f1dc0455
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-04-23 21:23:40 +0300

    Enchance Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,30 @@ import About from './About'
 
 function Header() {
     return (
-        <Router>
-            <nav>
-                <ul className='header-list'>
-                    <li className='active-choise'><Link to="/">Home</Link></li>
-                    <li><Link to="/current">Current</Link></li>
-                    <li><Link to="/statistic">Statistic</Link></li>
-                    <li className='float-r'><Link to="/about">About Us</Link></li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route exact path='/'><Home /></Route>
-                <Route path='/current'><Current /></Route>
-                <Route path='/statistic'><Statistic /></Route>
-                <Route path='/about'><About /></Route>
-            </Switch>
-      </Router>
+        <div>
+            <Router>
+                <nav>
+                    <ul className='header-list'>
+                        <li onClick={ changeActive } className='active-choise'><Link to="/">Home</Link></li>
+                        <li onClick={ changeActive }><Link to="/current">Current</Link></li>
+                        <li onClick={ changeActive }><Link to="/statistic">Statistic</Link></li>
+                        <li onClick={ changeActive } className='float-r'><Link to="/about">About Us</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path='/'><Home /></Route>
+                    <Route path='/current'><Current /></Route>
+                    <Route path='/statistic'><Statistic /></Route>
+                    <Route path='/about'><About /></Route>
+                </Switch>
+            </Router>
+        </div>
     )
+}
+
+function changeActive(e) {
+    document.getElementsByClassName('active-choise')[0].classList.remove('active-choise')
+    e.target.classList.add('active-choise')
 }
 
 export default Header;
